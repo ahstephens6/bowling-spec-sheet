9f3177a9b52fb72b74a25bf3dd9f14587febf926
ahstephens6 <asthesixth@icloud.com>
2025-08-24 15:40:11 -0400

    Made bowlers page and navigating to bowlers spec sheet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { SpecSheet } from './components/spec-sheet/spec-sheet';
-import { App } from './app';
+import { BowlerPage } from './components/bowler-page/bowler-page';
 
 export const routes: Routes = [
-    {path: '', component: App, title: 'Home'},
+    {path: '', component: BowlerPage, title: 'Bowlers'},
     {path: 'spec-sheet/:id', component: SpecSheet, title: 'Spec-Sheet'},
 ];
