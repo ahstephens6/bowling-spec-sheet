8edaa6c95d6049318f6fceb42b2e68badf7c99c1
ahstephens6 <asthesixth@icloud.com>
2025-08-24 15:33:09 -0400

    Added bowler page component

---
+++ src/app/component/bowler-page/bowler-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bowler-page',
+  imports: [],
+  templateUrl: './bowler-page.html',
+  styleUrl: './bowler-page.scss'
+})
+export class BowlerPage {
+
+}
